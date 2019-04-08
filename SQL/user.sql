a3e2c052aa0a0e7beba53a267169974774d4848b
MasaruTakahashi <MasaruTakahashi@users.noreply.github.com>
2019-04-08 18:16:40 +0900

    課題中

--- SQL/user.sql
+++ SQL/user.sql
@@ -1,2 +1,13 @@
-CREATE DATABASE user DEFAULT CHARACTER SET utf8;
-drop db user;
+CREATE DATABASE webdb DEFAULT CHARACTER SET utf8;
+use webdb;
+create table user(
+id SERIAL PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
+login_id varchar(255) UNIQUE NOT NULL,
+name varchar(255) NOT NULL,
+birth_date DATE NOT NULL,
+password varchar(255) NOT NULL,
+create_date DATETIME NOT NULL,
+update_date DATETIME NOT NULL);
+select * from user;
+insert into user values(1,'admin','ä«óùé“','1991-03-20','aaa',now(),now());
+
